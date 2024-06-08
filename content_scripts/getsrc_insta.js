93a5b92c3aaa281eb2e427a5143772333eec2218
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-06-08 17:49:20 +0100

    Added Video Thumbnail support to Instagram downloader

--- content_scripts/getsrc_insta.js
+++ content_scripts/getsrc_insta.js
@@ -22,24 +22,37 @@ function handleUserAction(message) {
 	log(`received request for '${message.command}'`);
 	if (message.command == "extract_image_url") {
 		imageUrl = getImageUrl();
-		log(`got image URL ${imageUrl}`)
-		message.url = imageUrl
+		log(`got image URL ${imageUrl}`);
+		message.url = imageUrl;
 		notifyBackground(message);
 	}
 	else {
-		onError(`unknown command: ${message.command}`)
+		onError(`unknown command: ${message.command}`);
 	}
 }
 
 /** Extract the image from the current Instagram page */
 function getImageUrl() {
+	// First check for a main content image
+	log("Checking for main Content Image")
 	imgElement = document.querySelector("div[style^='padding'] > img[src]");
-	if (!imgElement) {
-		onError("no images found!")
-		return null
+	if (imgElement) {
+		imgUrl = imgElement.src;
+		return imgUrl;
 	}
-	imgUrl = imgElement.src
-	return imgUrl;
+	
+	log("Main Image Element not found; checking for Video Thumbnail")
+
+	// If image check failed, then check for a video thumbnail
+	vidElement = document.querySelector("div:not([id='splash-screen']) > img[src]")
+	
+	if (!vidElement) {
+		onError("no images found!");
+		return null;
+	}
+
+	log("Found Video Thumbnail Image!")
+	return vidElement.src;
 }
 
 /** Register for window actions */
